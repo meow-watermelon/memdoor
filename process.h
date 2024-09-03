855623e669504ec53857aeee0d61ab411ae220f2
Hui Li <herdingcat@yahoo.com>
2024-09-02 21:36:12 -0400

    1.4.0 - issue#10
    Signed-off-by: Hui Li <herdingcat@yahoo.com>

--- process.h
+++ process.h
@@ -1,6 +1,9 @@
 #ifndef PROCESS_H
 #define PROCESS_H
 
+#include <limits.h>
+#include <sys/types.h>
+
 struct meminfo {
     int process_oom_score;
     int process_oom_score_adj;
@@ -10,10 +13,13 @@ struct meminfo {
 };
 
 extern int check_pid(pid_t pid);
-extern int compare_pid_exe(pid_t, char *exe_name);
-extern int get_oom_score(pid_t pid, struct meminfo *input_meminfo);
-extern int get_memory_usage(pid_t pid, struct meminfo *input_meminfo);
-extern int get_system_memory(struct meminfo *input_meminfo);
+extern int get_ppid(pid_t pid, int *ppid, char *exe_name);
+extern char *get_exe_path_name(pid_t pid);
+extern int compare_pid_exe(pid_t pid, char *exe_name);
+extern int get_oom_score(pid_t pid, int *oom_score, int *oom_score_adj);
+extern int get_memory_usage(pid_t pid, long int *process_rss, long int *process_pss);
+extern int get_system_memory(long int *total_memory);
+extern void get_process_tree(pid_t pid);
 extern void get_memory_mapping(pid_t pid);
 extern void get_network_connection(pid_t pid);
 
